c43d27ab6085a07c3f47119ddbc7582b6fe2869c
italo <italocaua19506@gmail.com>
2025-05-01 19:04:19 -0300

    Adicao da opcao de copia de arquivos

--- buscador_de_arquivos.py
+++ buscador_de_arquivos.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 from colorama import Fore, Back, Style
 from time import sleep
 
@@ -20,14 +21,23 @@ caminho_original = os.path.normpath(str(input("Digite o caminho da pasta em que 
 caminho_destino = str(input("Digite o caminho de destino para onde irão os arquivos: "))
 extensao_procurada = str(input("Digite a extensão do arquivo que gostaria que o programa rastreasse: ")).lower()
 
+moveOrCopy = int(input("[0] Mover arquivos\n[1] Copiar arquivos\n"))
+
+if moveOrCopy not in [0, 1]:
+    print("❌ Opção inválida. Encerrando...")
+    exit()
+
+if moveOrCopy == 1:
+    moveOrCopy = True
+else:
+    moveOrCopy = False
+
 # Percorrendo recursivamente os diretórios e arquivos a partir do caminho especificado pelo usuário
 loading(3, 0.5)
 for pasta_atual, _, arquivos in os.walk(caminho_original):
     sleep(0.4)
-    print("")
     print(f"Pasta da vez: {pasta_atual}")
     print(f"Arquivos contidos: {arquivos}")
-    print("")
 
     # Verifica se há pelo menos 1 arquivo com a extensão desejada na pasta_atual
     existe_arquivo = any(arquivo.endswith(extensao_procurada) for arquivo in arquivos)
@@ -35,7 +45,7 @@ for pasta_atual, _, arquivos in os.walk(caminho_original):
         continue
 
     for arquivo in arquivos:
-        if arquivo.endswith(f"{extensao_procurada}"):
+        if arquivo.lower().endswith(f"{extensao_procurada}"):
             print(f"Lendo: {arquivo}", end="")
 
             # Verificando se o nome do arquivo já existe no destino
@@ -45,14 +55,22 @@ for pasta_atual, _, arquivos in os.walk(caminho_original):
                 continue
 
             # Verificando se foi possível mover o arquivo
-            try:
-                origem= os.path.join(pasta_atual, arquivo)
-                destino = os.path.join(caminho_destino, arquivo)
-                os.rename(origem, destino)
-                print("")
-                print(f"Tranferindo: {arquivo}...", end="")
-            except (FileNotFoundError, PermissionError, OSError) as error:
-                print(f"\n❌ Erro ao mover o arquivo '{arquivo}': {error}")
+            if not moveOrCopy:
+                try:
+                    origem= os.path.join(pasta_atual, arquivo)
+                    destino = os.path.join(caminho_destino, arquivo)
+                    os.rename(origem, destino)
+                    print(f"Tranferindo: {arquivo}...", end="")
+                except (FileNotFoundError, PermissionError, OSError) as error:
+                    print(f"\n❌ Erro ao mover o arquivo '{arquivo}': {error}")
+            if moveOrCopy:
+                try:
+                    origem= os.path.join(pasta_atual, arquivo)
+                    destino = os.path.join(caminho_destino, arquivo)
+                    shutil.copy2(origem, destino)
+                    print(f"Copiando: {arquivo}...", end="")
+                except (FileNotFoundError, PermissionError, OSError) as error:
+                    print(f"\n❌ Erro ao copiar o arquivo '{arquivo}': {error}")
 
 
 
